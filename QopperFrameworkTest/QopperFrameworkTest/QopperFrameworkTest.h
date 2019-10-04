fb14e01ed4308a9d70cad6d189d49f13a5c12ffd
Mac <mac@Macs-iMac.local>
2019-10-04 11:08:57 +0530

    first commit

---
+++ QopperFrameworkTest/QopperFrameworkTest/QopperFrameworkTest.h
@@ -0,0 +1,19 @@
+//
+//  QopperFrameworkTest.h
+//  QopperFrameworkTest
+//
+//  Created by Mac on 04/10/19.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QopperFrameworkTest.
+FOUNDATION_EXPORT double QopperFrameworkTestVersionNumber;
+
+//! Project version string for QopperFrameworkTest.
+FOUNDATION_EXPORT const unsigned char QopperFrameworkTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QopperFrameworkTest/PublicHeader.h>
+
+
---
+++ QopperFrameworkTest/QopperFrameworkTest/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  QopperFrameworkTest
+//
+//  Created by Mac on 04/10/19.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import Foundation
+public class Service {
+    private init(){
+        
+    }
+    public static func doSomething() -> String {
+        return "Did Sime stuff here we go "
+    }
+}
